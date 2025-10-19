5524fce07419ffce6e367fb5909859906e5e5f50
onasito <oarrechavala0@gmail.com>
2025-10-18 18:30:06 -0700

    Fixed verifyToken from previous commit error.

--- server/src/middleware/verifyToken.js
+++ server/src/middleware/verifyToken.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 
 function verifyToken (req, res, next) {
-const verifyToken = (req, res, next) => {
     // Get token from authorization header
     const token = req.headers['authorization'];
     if (!token) {
