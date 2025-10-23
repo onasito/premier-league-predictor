0cd24c31f98c74bfcb27249276ce72802a9f6831
onasito <oarrechavala0@gmail.com>
2025-10-22 23:07:38 -0700

    began prediction routes

--- server/src/routes/predictionRoutes.js
+++ server/src/routes/predictionRoutes.js
@@ -5,3 +5,10 @@ import verifyToken from '../middleware/verifyToken.js';
 const router = express.Router();
 
 // Get predictions for authenticated user
+router.post('/predictions', verifyToken, async (req, res) => {
+    try {
+        const {matchId, predictedWinner} = req.body;
+    } catch (error) {
+        
+    }
+})
